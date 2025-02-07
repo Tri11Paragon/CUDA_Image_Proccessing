52b8771175806fabf676b08a056940cc92bc117a
Brett Laptop <tri11paragon@tpgc.me>
2025-02-06 19:31:48 -0500

    way more images

--- image_preprocessor.py
+++ image_preprocessor.py
@@ -68,12 +68,121 @@ def add_poisson_noise(image):
     return noisy
 
 
-def run_generate(args):
-    files = list(Path(args.res_folder).iterdir())
-    for file in files:
-        if file.is_folder():
-            files.append()
+def image_to_database(image, path, original, cursor):
+    thresh = process.threshold_image(image)
+    thresh_grey = cv2.cvtColor(thresh, cv2.COLOR_BGR2GRAY)
+    bounds = process.find_bounds_and_contours(thresh_grey)
 
+    cursor.execute("""
+                INSERT INTO images (filename, original_file)
+                VALUES (?, ?)
+            """, (path, original))
+
+    image_id = cursor.lastrowid
+    print(image_id)
+
+    for bound, con in bounds:
+        x, y, w, h = bound
+        serialized_contour = pickle.dumps(con)
+
+        cursor.execute("""
+                INSERT INTO contours (image_id, contour)
+                VALUES (?, ?)
+            """, (image_id, serialized_contour))
+
+        cursor.execute("""
+                INSERT INTO bounds (image_id, x, y, width, height)
+                VALUES (?, ?, ?, ?, ?)
+            """, (image_id, x, y, w, h))
+
+
+
+def run_generate(args):
+    Path(args.output_folder).mkdir(parents=True, exist_ok=True)
+    cwd = Path.cwd()
+
+    if args.d:
+        connection = sqlite3.connect(Path(args.output_folder).joinpath("image_data.db"))
+        cursor = connection.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS images (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                filename TEXT,
+                original_file TEXT)
+        """)
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS bounds (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                image_id INTEGER,
+                x INTEGER,
+                y INTEGER,
+                width INTEGER,
+                height INTEGER,
+                FOREIGN KEY(image_id) REFERENCES images(id)
+            )
+        """)
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS contours (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                image_id INTEGER,
+                contour BLOB,
+                FOREIGN KEY(image_id) REFERENCES images(id)
+            )
+        """)
+        connection.commit()
+
+    files = list(Path(args.res_folder).iterdir())
+    i = 0
+    file_name = 0
+    while i < len(files):
+        file = files[i]
+        i += 1
+        if file.is_dir():
+            files.extend(list(Path(file.resolve()).iterdir()))
+        else:
+            image = cv2.imread(str(file.resolve()))
+            h, v, b = generate_flipped(image)
+
+            new_image_name = Path(args.output_folder).joinpath(file.with_suffix('.' + args.t).name)
+
+            if args.u:
+                new_image_name = new_image_name.with_stem(str(file_name))
+                file_name += 1
+
+            if args.i:
+                new_image_name = new_image_name.with_stem(new_image_name.stem + "_" + file.parents[0].name)
+
+            if args.c:
+                cv2.imwrite(str(new_image_name.resolve()), image)
+                if args.d:
+                    image_to_database(image, str(new_image_name.resolve().relative_to(cwd)), str(file.resolve().relative_to(cwd)), cursor)
+                    connection.commit()
+
+            if args.a or args.z:
+                h_path = new_image_name.with_stem(new_image_name.stem + "_hflip").resolve()
+                cv2.imwrite(str(h_path), h)
+                if args.d:
+                    image_to_database(h, str(h_path.relative_to(cwd)), str(file.resolve().relative_to(cwd)), cursor)
+                    connection.commit()
+            if args.a or args.v:
+                v_path = new_image_name.with_stem(new_image_name.stem + "_vflip").resolve()
+                cv2.imwrite(str(v_path), v)
+                if args.d:
+                    image_to_database(v, str(v_path.relative_to(cwd)), str(file.resolve().relative_to(cwd)), cursor)
+                    connection.commit()
+            if args.a or args.b:
+                b_path = new_image_name.with_stem(new_image_name.stem + "_bflip").resolve()
+                cv2.imwrite(str(b_path), b)
+                if args.d:
+                    image_to_database(b, str(b_path.relative_to(cwd)), str(file.resolve().relative_to(cwd)), cursor)
+                    connection.commit()
+
+            print(f"Processed {new_image_name}")
+
+    if args.d:
+        connection.close()
 
 
 
@@ -89,8 +198,9 @@ def main():
     generate_parser.add_argument('-b', action='store_true', default=False, help="Generate images with both flips applied")
     generate_parser.add_argument('-c', action='store_true', default=False, help="Copy original image as well")
     generate_parser.add_argument('-u', action='store_true', default=False, help="Give images a unique id instead of their existing filename")
+    generate_parser.add_argument('-i', action='store_true', default=False, help="Insert the last directory name into the name of the image. Good for use with -u")
     generate_parser.add_argument('-t', action='store', default='jpg', help="Image type to output")
-    generate_parser.add_argument("res_folder", help="Folder to take images from, will maintain folder structure")
+    generate_parser.add_argument("res_folder", help="Folder to take images from")
     generate_parser.add_argument("output_folder", help="Folder to put the generated images into")
 
     args = parser.parse_args()
